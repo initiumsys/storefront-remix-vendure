4a3a8eae94930d47913776ca59718d85439c930d
aferreroc <aferrero@initiumsys.com>
2023-07-14 14:59:12 +0200

    apuntando a https://api_tienda.initiumsys.com/

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
 export const APP_META_TITLE = 'Tienda de Alberto';
 export const APP_META_DESCRIPTION = 'Tienda de Alberto';
 //export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
-export const DEMO_API_URL = 'https://d2d9-188-127-177-222.ngrok-free.app/shop-api';
+export const DEMO_API_URL = 'https://api_tienda.initiumsys.com//shop-api';
