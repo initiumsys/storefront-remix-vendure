3597a7f1c8ec5a8c139266da1cfc096a1fb6a5f8
aferreroc <aferrero@initiumsys.com>
2023-07-14 15:04:39 +0200

    reintento

--- app/constants.ts
+++ app/constants.ts
@@ -2,3 +2,4 @@ export const APP_META_TITLE = 'Tienda de Alberto';
 export const APP_META_DESCRIPTION = 'Tienda de Alberto';
 //export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
 export const DEMO_API_URL = 'https://api_tienda.initiumsys.com//shop-api';
+
