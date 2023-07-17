36278fb0c10ca24c6f2015b79045e05a438b6f56
aferreroc <aferrero@initiumsys.com>
2023-07-17 17:14:59 +0200

    cambiada url vendure

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,5 @@
 export const APP_META_TITLE = 'Tienda de Alberto';
 export const APP_META_DESCRIPTION = 'Tienda de Alberto';
-export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
-//export const DEMO_API_URL = 'https://api_tienda.initiumsys.com//shop-api';
+//export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
+export const DEMO_API_URL = 'https://api_tienda.initiumsys.com//shop-api';
 
