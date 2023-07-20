d5a3dee67bfeb7ae9c3c0e0a800c7a4f32930901
aferreroc <aferrero@initiumsys.com>
2023-07-20 21:47:44 +0200

    añadido channel token

--- app/constants.ts
+++ app/constants.ts
@@ -1,7 +1,6 @@
 export const APP_META_TITLE = 'Tienda de Alberto';
 export const APP_META_DESCRIPTION = 'Tienda de Alberto';
 //export const DEMO_API_URL = 'https://readonlydemo.vendure.io/shop-api';
-export const DEMO_API_URL = 'https://api_tienda.initiumsys.com/shop-api?vendure-token=Yk1PwR2v8fXF4hmV44
-';
+export const DEMO_API_URL = 'https://api_tienda.initiumsys.com/shop-api?vendure-token=Yk1PwR2v8fXF4hmV44';
 export const DEMO_STRIPE_PUBLISHABLE_KEY='pk_test_51HkSXfBul6Klpy5OJHKWmStliauH5D5xViBLKVYkLdClW96Vi2WOGxzm2m8K9xO1ixiD9gqFU731VtFbGbXz6P8w005ZvZpaJl';
 
